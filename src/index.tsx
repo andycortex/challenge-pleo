9853fdb5c77ce7284bd365f1fd60692850dcaca6
Cortex <cortex.andy2809@gmail.com>
2022-10-26 18:26:16 -0400

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { Provider as FavoriteProvider } from "./context/favorite"
+
+import App from "./components/app";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProvider>
+        <CSSReset />
+        <FavoriteProvider>
+          <App />
+        </FavoriteProvider>
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
